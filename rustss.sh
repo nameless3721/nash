5b258e8ec8666f49a75b252b3717b507eb3d9d68
nameless3721 <50364156+nameless3721@users.noreply.github.com>
2020-11-10 04:52:07 +0900

    Update rustss.sh

--- rustss.sh
+++ rustss.sh
@@ -5,6 +5,8 @@ CONF="/etc/rustss/rustss-server.json"
 SYSTEMD="/etc/systemd/system/rustss.service"
 apt-get -y install nano wget
 read -p "输入端口" port
+read -p "输入密码或者随机" shadowsockspwd
+[ -z "${shadowsockspwd}" ] && shadowsockspwd='${PSK}'
 cd ~/
 wget --no-check-certificate -O rustss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.8.23/shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz
 tar -xvJf rustss.tar.xz
@@ -31,7 +33,7 @@ if [ -f ${CONF} ]; then
   echo "{" >>${CONF}
   echo "    \"server\": \"0.0.0.0\"," >>${CONF}
   echo "    \"server_port\": $port," >>${CONF}
-  echo "    \"password\": \"${PSK}\"," >>${CONF}
+  echo "    \"password\": \"$shadowsockspwd\"," >>${CONF}
   echo "    \"timeout\": 60," >>${CONF}
   echo "    \"mode\":\"tcp_and_udp\"," >>${CONF}
   echo "    \"method\": \"rc4-md5\"" >>${CONF}
