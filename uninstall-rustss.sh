620e124fb7e084ed79c7a51d6e690e1042c9f17f
nameless3721 <50364156+nameless3721@users.noreply.github.com>
2020-11-10 00:04:44 +0900

    Create uninstall-rustss.sh

---
+++ uninstall-rustss.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+systemctl stop rustss
+systemctl disable rustss
+rm -f /etc/systemd/rustss.service
+rm -f /usr/bin/ssserver
+rm -f /usr/bin/sslocal
+rm -f /usr/bin/ssmanager
+rm -f /usr/bin/ssurl
+rm -f /etc/rustss/rustss-server.conf
+rm -rf /etc/rustss/
