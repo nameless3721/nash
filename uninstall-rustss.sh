76c0cdb4ba1d7c8490953072bd3ef37ee84c8943
nameless3721 <50364156+nameless3721@users.noreply.github.com>
2020-11-10 04:24:56 +0900

    Update uninstall-rustss.sh

--- uninstall-rustss.sh
+++ uninstall-rustss.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 systemctl stop rustss
 systemctl disable rustss
-rm -f /etc/systemd/rustss.service
+rm -f /etc/systemd/system/rustss.service
 rm -f /usr/bin/ssserver
 rm -f /usr/bin/sslocal
 rm -f /usr/bin/ssmanager
